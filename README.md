d3f5d6cbd12d7505bd3b7a1daf613a933d496513
Harvey Cambridge <Harvey.Cambridge@gmail.com>
2021-05-09 17:56:40 +0100

    Updated to 2.0.0

---
+++ README.md
@@ -0,0 +1,66 @@
+
+
+# FD Alert
+
+This addon gives alerts when your Feign Death resists. 
+- Sends a custom message to a chat channel (say, yell, raid)
+- Sends a custom message via whisper to any name
+- Plays a sound of your choice
+
+### Guide
+FD Alert uses a few slash commands to configure. By default it sends a message in /say, plays a bell sound, and does not send any whispers. You can change the messages (both channel and whisper messages can be different), and change which channel they send to or which person they whisper. The sound alert can be changed to one of 10 built in sounds, or any sound in the WoW files.
+
+Commands | Effect
+-----------|------------
+`/fda` or `/fda help` | Shows command list
+`/fda enable` | Toggle mod on/off
+`/fda message <message>` | Changes the main message, e.g `/fda message My FD resisted!`*
+`/fda channel <channel>` | Sets the main channel to announce in (say, yell, pary, raid, raid_warning, and numbers 1-9) or 0 to disable
+`/fda sound <number>` | Select a sound. (1-10) or 0 to disable**
+`/fda tally` | Whether FD Alert will tally your resists/attempts per fight
+`/fda test` | Simulate a FD resist based on your current settings
+`/fda status` | Display current settings
+`/fda default` | Reset settings to default
+
+
+*The string `%p` can be used to enter the player's name. For example the player named `Leeroy` would use 
+>`/fda message %p couldn't play dead well enough!` 
+
+which would equate to
+>`Leeroy couldn't play dead well enough!`
+
+(Just for you lazy people out there)
+
+**FD Alert has 10 sounds built in, but you can use any WoW sound you like by editing the saved variables folder, found in: WoW\WTF\Account\<account>\<server>\<character>\SavedVariables\FD_Alert.lua
+
+A WoW sound list can be found at http://www.wowwiki.com/API_PlaySoundFile_SoundList (warning, there are thousands of them. I just picked 10 random ones that sounded decent for the mod)
+
+You can turn off the channel messages or sound alert by setting them to "0".
+
+### Possible features
+
+- [ ] Simple GUI
+- [ ] Adding visual alert
+  
+### Changelog
+2.0.0
+- Simplified addon and neatened code
+- Added way to get player's name
+
+1.1.3
+- Added tracking of resists/attempts per fight
+- Added consecutive resists mentioned in chat
+
+----------------------------------
+
+1.1 and older made by Intermission
+1.1
+- fixed a couple of typos (didnt effect functionality)
+- added a readme.txt (that nobody will read anyway)
+- made some of the code slightly more sexier than before
+
+1.0
+- release, it works.
+----------------------------------
+Original by Intermission, on Frostmourne-US
+Modernised by CGN - https://github.com/BokChoyWarrior
--- fdalert.lua
+++ fdalert.lua
@@ -1,42 +1,34 @@
 -- FD Alert
--- by Intermission, Avast Guild, Frostmourne-US Server
--- with help from Nightblade
--- version 1.1.3
--- 14/08/2008
--- http://www.wowinterface.com/downloads/info10329-FDAlert.html
+-- by CGN, Netherwing-Atlantiss https://github.com/BokChoyWarrior
+-- version 2.0.0
+-- 09/05/2021
 
--- Modified by CGN to track resists/tries per fight
-
-local chat_prefix = "FD Alert: "
+local player_name = _G.UnitName("player")
 local colour = "|cffffff7f"
-
 local GetTime = _G.GetTime
 local fd_spell_string = _G.GetSpellInfo(5384)
 local fd_primed = 0
--- Resists = [Resists, Tries]
 local fd_outcomes = {resists = 0, tries = 0}
 local consecutive_resists = 0
 
 local sounds = {}
-sounds[1] = "Sound\\Creature\\Mortar Team\\MortarTeamPissed9.wav"
-sounds[2] = "Sound\\Creature\\Sporeling\\SporelingDeath.wav"
-sounds[3] = "Sound\\Creature\\Voidwalker_VoidWraith\\Voidwalker_VoidWraithAggro.wav"
-sounds[4] = "Sound\\Effects\\DeathImpacts\\mDeathImpactColossalSnowA.wav"
-sounds[5] = "Sound\\Effects\\DeathImpacts\\mDeathImpactLargeWoodA.wav"
-sounds[6] = "Sound\\Item\\Weapons\\Mace1HMetal\\1hMaceMetalHitChainCrit.wav"
-sounds[7] = "Sound\\Item\\Weapons\\ParrySounds\\1hParryMetalHitMetalCritical.wav"
-sounds[8] = "Sound\\Creature\\Abomination\\AbominationPissed5.wav"
-sounds[9] = "Sound\\Doodad\\BellTollHorde.wav"
-sounds[10] = "Sound\\Doodad\\BellTollAlliance.wav"
+sounds[1] = "Sound/Creature/Mortar Team/MortarTeamPissed9.wav"
+sounds[2] = "Sound/Creature/Sporeling/SporelingDeath.wav"
+sounds[3] = "Sound/Creature/Voidwalker_VoidWraith/Voidwalker_VoidWraithAggro.wav"
+sounds[4] = "Sound/Effects/DeathImpacts/mDeathImpactColossalSnowA.wav"
+sounds[5] = "Sound/Effects/DeathImpacts/mDeathImpactLargeWoodA.wav"
+sounds[6] = "Sound/Item/Weapons/Mace1HMetal/1hMaceMetalHitChainCrit.wav"
+sounds[7] = "Sound/Item/Weapons/ParrySounds/1hParryMetalHitMetalCritical.wav"
+sounds[8] = "Sound/Creature/Abomination/AbominationPissed5.wav"
+sounds[9] = "Sound/Doodad/BellTollHorde.wav"
+sounds[10] = "Sound/Doodad/BellTollAlliance.wav"
 
 function fda_Defaults()
 	local default_prefs = {
 		enabled = "on",
-		message = "My Feign Death RESISTED!",
+		message = "%p's feign death RESISTED!",
 		channel = "SAY",
-		whispermessage = "my fd broke, halp",
-		whispertarget = "0",
-		sound = "Sound\\Doodad\\BellTollAlliance.wav",
+		sound = "Sound/Doodad/BellTollAlliance.wav",
 		tally = "on",
 		debug = "off"
 		}
@@ -47,24 +39,17 @@ function fda_OnLoad()
 	this:RegisterEvent("UI_ERROR_MESSAGE")
 	this:RegisterEvent("ACTIONBAR_UPDATE_COOLDOWN")
 	this:RegisterEvent("VARIABLES_LOADED")
-
-	-- V CGN V --
 	this:RegisterEvent("UNIT_SPELLCAST_SENT")
 	this:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 	this:RegisterEvent("PLAYER_REGEN_ENABLED")
-	-- ^ CGN ^ --
 
 	SlashCmdList["FDALERT"] = function(msg) fda_OnSlash(msg) end
 	SLASH_FDALERT1 = "/fdalert"
 	SLASH_FDALERT2 = "/fda"
 
 	if not fdalert_Prefs then
-		fdalert_Prefs = fda_Defaults()
+		fda_ResetSettings()
 	end
-
-	-- added by CGN v v v v
-	-- Ideally we wouldnt have to do this, but the creator decided to raw get values from the fda_Prefs
-	-- rather than use a getter func that can set defaults if not existing :(
 
 	-- Check current saved values and fill in missing with defaults, because the user may have uypdated to a version with extra saved vars
 	local default_prefs = fda_Defaults()
@@ -76,24 +61,22 @@ function fda_OnLoad()
 end
 
 function fda_Print(msg)
-	DEFAULT_CHAT_FRAME:AddMessage( colour .. chat_prefix .. "|r" .. msg );
+	DEFAULT_CHAT_FRAME:AddMessage( colour .. "FD Alert: " .. "|r" .. msg )
 end
 
 function fda_DebugPrint(msg)
 	if fdalert_Prefs.debug == "on" then
-		DEFAULT_CHAT_FRAME:AddMessage( colour .. "FDA DEBUG: " .. "|r" .. msg );
+		DEFAULT_CHAT_FRAME:AddMessage( colour .. "FDA DEBUG: " .. "|r" .. msg )
 	end
 end
 
 function fda_OnSlash(msg)
 	if (not msg or msg == "") then
-		fda_Print("commands:")
-		fda_Print("/fda help    -- detailed help")
+		fda_Print("Commands:")
+		fda_Print("/fda help -- detailed help")
 		fda_Print("/fda enable")
 		fda_Print("/fda message <message>")
-		fda_Print("/fda chan <channel>")
-		fda_Print("/fda wm <whisper message>")
-		fda_Print("/fda wt <name>")
+		fda_Print("/fda channel <channel>")
 		fda_Print("/fda sound <number 1-10>")
 		fda_Print("/fda tally")
 		fda_Print("/fda test")
@@ -107,12 +90,8 @@ function fda_OnSlash(msg)
 		fda_SetSound(msg)
 	elseif(strsub(msg, 1, 7) == "message") then
 		fda_SetMessage(msg)
-	elseif(strsub(msg, 1, 4) == "chan") then
-		fda_SetChannel(msg)
-	elseif(strsub(msg, 1, 2) == "wm") then
-		fda_WMessageSet(msg)
-	elseif(strsub(msg, 1, 2) == "wt") then
-		fda_WTargetSet(msg)
+	elseif(strsub(msg, 1, 7) == "channel") then
+		fda_SetChannel(strsub(msg, 9))
 	elseif(msg == "status") then
 		fda_Status()
 	elseif(msg == "test") then
@@ -124,50 +103,46 @@ function fda_OnSlash(msg)
 	elseif(msg == "debug") then
 		fda_DebugToggle()
 	else
-		fda_Print("unknown option '" .. msg .. "' -- type '/fda' for help")
+		fda_Print("unknown option '" .. msg .. "' -- type '/fda help' for help")
 	end
 end
 
 -- v Functions added by CGN v --
-local wait_table = {};
-local wait_frame = nil;
+local wait_table = {}
+local wait_frame = nil
 
 function fda_Wait(delay,func, ...)
-	-- if(type(delay)~="number" or type(func)~="function") then
-	-- ThreatLib:Debug("delay false!")
-	-- return false;
-	-- end
 	if(wait_frame == nil) then
-		wait_frame = CreateFrame("Frame","wait_frame", UIParent);
+		wait_frame = CreateFrame("Frame","wait_frame", UIParent)
 		wait_frame:SetScript("onUpdate",function (self,elapse)
-			local count = #wait_table;
-			local i = 1;
+			local count = #wait_table
+			local i = 1
 			while(i<=count) do
-				local waitRecord = tremove(wait_table,i);
-				local d = tremove(waitRecord,1);
-				local f = tremove(waitRecord,1);
-				local p = tremove(waitRecord,1);
+				local waitRecord = tremove(wait_table,i)
+				local d = tremove(waitRecord,1)
+				local f = tremove(waitRecord,1)
+				local p = tremove(waitRecord,1)
 				if(d>elapse) then
-					tinsert(wait_table,i,{d-elapse,f,p});
-					i = i + 1;
+					tinsert(wait_table,i,{d-elapse,f,p})
+					i = i + 1
 				else
-					count = count - 1;
-					f(unpack(p));
+					count = count - 1
+					f(unpack(p))
 				end
 			end
-		end);
+		end)
 	end
-	tinsert(wait_table,{delay,func,{...}});
-	return true;
+	tinsert(wait_table,{delay,func,{...}})
+	return true
 end
 
 function fda_TryReset()
 	if GetTime() - fd_primed < 5 then
-		fda_DebugPrint("FD Success")
+		fda_DebugPrint("FD success")
 		fd_primed = 0
 		consecutive_resists = 0
 	else
-		fda_DebugPrint("FD must have resisted")
+		fda_DebugPrint("FD resisted")
 	end
 end
 
@@ -177,19 +152,18 @@ function fda_OnEvent()
 			fdalert_Prefs = fda_Defaults()
 		end
 	end
-	if(event == "UI_ERROR_MESSAGE") then
+	if event == "UI_ERROR_MESSAGE" then
 		-- one of the next two lines should be commented out.
-		if(arg1 == ERR_FEIGN_DEATH_RESISTED) then
+		if arg1 == ERR_FEIGN_DEATH_RESISTED then
 		-- if(arg1 == INTERRUPTED or arg1 == ERR_FEIGN_DEATH_RESISTED) then      -- for easy testing using dismiss pet + moving
-			-- v CGN v --
 			fd_outcomes.resists = fd_outcomes.resists + 1
 			consecutive_resists = consecutive_resists + 1
 			fd_primed = 0
 			fda_DebugPrint(tostring(consecutive_resists) .. " " .. tostring(fd_outcomes.resists))
-			-- ^ CGN ^ --
+
 			if(tostring(fdalert_Prefs.enabled) ==  "on") then
 				fda_SoundAlert()
-				fda_SendMessages()
+				fda_SendMessage()
 			end
 		end
 	end
@@ -222,7 +196,7 @@ function fda_OnEvent()
 end
 
 function fda_BuildChatMessage()
-	local msg = fdalert_Prefs.message
+	local msg = fdalert_Prefs.message:gsub("%%p", player_name)
 	if fdalert_Prefs.tally == "on" then
 		if consecutive_resists > 1 then
 			msg = msg .. " (" .. tostring(consecutive_resists) .. " in a row)"
@@ -232,47 +206,55 @@ function fda_BuildChatMessage()
 end
 
 function fda_Test()
-	if(tostring(fdalert_Prefs.enabled) ==  "on") then
+	if fdalert_Prefs.enabled ==  "on" then
 		fda_SoundAlert()
-		fda_SendMessages()
+		fda_SendMessage()
 	end
 end
 
 function fda_SoundAlert()
-	if(tostring(fdalert_Prefs.sound) ~= "0") then
-		PlaySoundFile(tostring(fdalert_Prefs.sound));
+	if fdalert_Prefs.sound ~= "0" then
+		PlaySoundFile(tostring(fdalert_Prefs.sound))
 	end
 end
 
-function fda_SendMessages()
+function fda_SendMessage()
 	msg = fda_BuildChatMessage()
-	local channelwork = tostring(fdalert_Prefs.channel)
-	if(channelwork == "1" or channelwork == "2" or channelwork == "3" or channelwork == "4" or channelwork == "5" or channelwork == "6" or channelwork == "7" or channelwork == "8" or channelwork == "9") then
-		SendChatMessage(msg, "CHANNEL", nil, tostring(fdalert_Prefs.channel));
-	elseif(channelwork == "0") then
+	local announce_chanel = fdalert_Prefs.channel
+	if announce_chanel == "0" then
+		return
+	elseif (
+		announce_chanel == "1" or
+		announce_chanel == "2" or
+		announce_chanel == "3" or
+		announce_chanel == "4" or
+		announce_chanel == "5" or
+		announce_chanel == "6" or
+		announce_chanel == "7" or
+		announce_chanel == "8" or
+		announce_chanel == "9"
+	) then
+		SendChatMessage(msg, "CHANNEL", nil, fdalert_Prefs.channel)
 	else
-		SendChatMessage(msg, tostring(fdalert_Prefs.channel));
-	end
-	local whispertargetwork = tostring(fdalert_Prefs.whispertarget)
-	if(whispertargetwork ~= "0") then
-		SendChatMessage(tostring(fdalert_Prefs.whispermessage), "WHISPER", nil, tostring(fdalert_Prefs.whispertarget));
+		SendChatMessage(msg, tostring(fdalert_Prefs.channel))
 	end
 end
 
 function fda_EnableToggle()
-	if(fdalert_Prefs.enabled == "off") then
-		fda_Print("addon enabled");
-		fdalert_Prefs.enabled = "on";
+	if fdalert_Prefs.enabled == "off" then
+		fda_Print("FD Alert enabled")
+		fdalert_Prefs.enabled = "on"
   		return
   else
-	fda_Print("addon disabled");
-  	fdalert_Prefs.enabled = "off";
+	fda_Print("FD Alert disabled")
+  	fdalert_Prefs.enabled = "off"
   end
 end
 
 function fda_SetMessage(msg)
-	fda_Print("message changed to: " .. strsub(msg, 9));
-	fdalert_Prefs.message = strsub(msg, 9);
+	msg = msg:gsub("%%p", player_name)
+	fda_Print("Message changed to: `" .. tostring(strsub(msg, 9)) .. "`")
+	fdalert_Prefs.message = strsub(msg, 9)
 end
 
 function fda_SetSound(msg)
@@ -280,93 +262,89 @@ function fda_SetSound(msg)
 	local sound_number = tonumber(sound)
 	local number_of_sounds = #sounds
 	fda_DebugPrint("sound, sound_number, num_of_sounds = " .. tostring(sound) .. " ".. tostring(sound_number) .. " " .. tostring(number_of_sounds))
-	if(sound == "0" or sound == "off" or sound == "disable" or sound_number == nil) then
+	if sound == "0" or sound == "off" or sound == "disable" or sound_number == nil then
 		fdalert_Prefs.sound = "0"
 		fda_Print("Sound turned off")
 	elseif sound_number > 0 and sound_number <= number_of_sounds then
 		fdalert_Prefs.sound = sounds[sound_number]
-		fda_Print("sound file changed to: " .. fdalert_Prefs.sound)
+		fda_Print("Sound file changed to: " .. fdalert_Prefs.sound)
 		fda_SoundAlert()
 	else
 		fda_Print("'" .. sound .. "' not recognised. Valid choices are 1 to " .. number_of_sounds .. ", or 0 for off.")
 	end
 end
 
-function fda_SetChannel(msg)
-	if(strsub(msg, 6) == "say" or strsub(msg, 6) == "yell" or strsub(msg, 6) == "emote" or strsub(msg, 6) == "party" or strsub(msg, 6) == "guild" or strsub(msg, 6) == "officer" or strsub(msg, 6) == "raid" or strsub(msg, 6) == "raid_warning" or strsub(msg, 6) == "1" or strsub(msg, 6) == "2" or strsub(msg, 6) == "3" or strsub(msg, 6) == "4" or strsub(msg, 6) == "5" or strsub(msg, 6) == "6" or strsub(msg, 6) == "7" or strsub(msg, 6) == "8" or strsub(msg, 6) == "9" or strsub(msg, 6) == "0") then
-		fda_Print("channel changed to: " .. strsub(msg, 6));
-		fdalert_Prefs.channel = string.upper((strsub(msg, 6)));
-  	return
-  else
-  	fda_Print("'" .. msg .. "' is not a valid channel name or number. Valid channels are:");
-  	fda_Print("say, emote, yell, party, raid, raid_warning, guild, officer, 1-9. '0' to disable the message.");
-  	return
-  end
+function fda_SetChannel(given_channel)
+	if (
+		given_channel == "say" or
+		given_channel == "yell" or
+		given_channel == "party" or
+		given_channel == "raid" or
+		given_channel == "raid_warning" or
+		given_channel == "1" or
+		given_channel == "2" or
+		given_channel == "3" or
+		given_channel == "4" or
+		given_channel == "5" or
+		given_channel == "6" or
+		given_channel == "7" or
+		given_channel == "8" or
+		given_channel == "9" or
+		given_channel == "0"
+	) then
+		fda_Print("Channel changed to: " .. given_channel)
+		fdalert_Prefs.channel = string.upper(given_channel)
+  		return
+  	else
+		fda_Print("'" .. given_channel .. "' is not a valid channel name or number. Valid channels are:")
+		fda_Print("say, yell, party, raid, raid_warning, 1-9. '0' to disable the message.")
+  	end
 end
 
-function fda_WMessageSet(msg)
-	fda_Print("whisper message changed to: " .. strsub(msg, 4));
-	fdalert_Prefs.whispermessage = strsub(msg, 4);
-  return
-end
-
-function fda_WTargetSet(msg)
-	fda_Print("whisper target changed to: " .. strsub(msg, 4));
-	fdalert_Prefs.whispertarget = strsub(msg, 4);
-  return
-end
-
--- v CGN v --
 function fda_TallyToggle()
-	if (fdalert_Prefs.tally == "off") then
-		fda_Print("tallying enabled");
-		fdalert_Prefs.tally = "on";
+	if fdalert_Prefs.tally == "off" then
+		fda_Print("tallying enabled")
+		fdalert_Prefs.tally = "on"
 		return
 	else
-		  fda_Print("tally disabled");
-		fdalert_Prefs.tally = "off";
+		  fda_Print("tally disabled")
+		fdalert_Prefs.tally = "off"
 	end
 end
 
 function fda_DebugToggle()
-	if (fdalert_Prefs.debug == "off") then
-		fdalert_Prefs.debug = "on";
-		fda_DebugPrint("debug enabled");
+	if fdalert_Prefs.debug == "off" then
+		fdalert_Prefs.debug = "on"
+		fda_DebugPrint("debug enabled")
 		return
 	else
-		fda_Print("debug disabled");
-		fdalert_Prefs.debug = "off";
+		fda_Print("debug disabled")
+		fdalert_Prefs.debug = "off"
 	end
 end
--- ^ CGN ^ --
 
 function fda_Status()
-	fda_Print("current mod settings:");
-	fda_Print("/fda enable = " .. tostring(fdalert_Prefs.enabled));
-	fda_Print("/fda message = " .. tostring(fdalert_Prefs.message));
-	fda_Print("/fda chan = " .. string.lower(tostring(fdalert_Prefs.channel)));
-	fda_Print("/fda wm = " .. tostring(fdalert_Prefs.whispermessage));
-	fda_Print("/fda wt = " .. tostring(fdalert_Prefs.whispertarget));
-	fda_Print("/fda sound = " .. tostring(fdalert_Prefs.sound));
-	fda_Print("/fda tally = " .. tostring(fdalert_Prefs.tally));
+	fda_Print("FD Alert settings:")
+	fda_Print("/fda enable = " .. tostring(fdalert_Prefs.enabled))
+	fda_Print("/fda message = " .. tostring(fdalert_Prefs.message))
+	fda_Print("/fda channel = " .. string.lower(tostring(fdalert_Prefs.channel)))
+	fda_Print("/fda sound = " .. tostring(fdalert_Prefs.sound))
+	fda_Print("/fda tally = " .. tostring(fdalert_Prefs.tally))
 end
 
 function fda_ShowHelp()
-	fda_Print("help:")
-	fda_Print("/fda help  = brings up this help list")
-	fda_Print("/fda enable  = toggles the mod on/off")
-	fda_Print("/fda message <message>  = sets the message that will be sent to the channel")
-	fda_Print("/fda chan <channel>  = sets the channel that the message will be sent to. Valid chat channels: say, emote, yell, party, raid, raid_warning, guild, officer, and the channel numbers 1 to 9. Set to '0' to disable channel messages.")
-	fda_Print("/fda wm <whisper message>  = sets the message that will be sent in whispers")
-	fda_Print("/fda wt <name>  = sets the target for the whisper. Set to '0' to disable whisper messages.")
-	fda_Print("/fda sound <number>  = selects a sound. Valid choices are 1-10. Set to '0' to disable sound.")
-	fda_Print("/fda tally  = Changes whether FDA will tally your resists.")
-	fda_Print("/fda test  = simulates an FD resist based on your current settings")
-	fda_Print("/fda status  = displays your current FD Alert configuration")
-	fda_Print("/fda reset  = resets all settings back to their default state")
+	fda_Print("/fda  = brings up this help list")
+	fda_Print("/fda enable  = Toggle addon on/off")
+	fda_Print("/fda message <message>  = Set the message that will be sent to the channel")
+	fda_Print("/fda channel <channel>  = Set the channel to send message in. Valid: say, yell, party, raid, raid_warning, and the channel numbers 1 to 9. - '0' to disable.")
+	fda_Print("/fda sound <number>  = Select a sound. Valid choices are 1-10. - '0' to disable sound")
+	fda_Print("/fda tally  = Change whether FDA will tally your resists")
+	fda_Print("/fda test  = Simulate a FD resist based on your current settings")
+	fda_Print("/fda status  = Display your current FD Alert configuration")
+	fda_Print("/fda reset  = Reset all settings back to their default state")
 end
 
 function fda_ResetSettings()
-	fda_Print("settings have been reset to default values, type '/fda status' to check settings");
-	fda_Defaults()
+	fda_Print("Settings have been reset to default values, type '/fda status' to check settings")
+	fdalert_Prefs = fda_Defaults()
 end
